c5dd4c5972673ae3f3155fb020d740617a300f16
Jarold Wong <jaroldw@gmail.com>
2015-12-02 11:18:10 -0800

    Implement updating posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,9 +22,27 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:notice] = "Post has been updated."
+      redirect_to @post
+    else
+      flash.now[:alert] = "Post has not been updated."
+      render 'edit'
+    end
+  end
+
+
   private
 
   def post_params
     params.require(:post).permit(:title, :name, :email, :description, :category)
   end
+
 end
