b41ea0b1917577c856417b459755c2cbd3d509e3
Jarold Wong <jaroldw@gmail.com>
2015-12-02 15:40:54 -0800

    Add queries for main page display by categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  def self.lost
+    where(category: 'lost')
+  end
+
+  def self.found
+    where(category: 'found')
+  end
+
+  def self.free
+    where(category: 'free')
+  end
+
+  def self.wanted
+    where(category: 'wanted')
+  end
 end
